bf9e5d9727e7192a93775ce6df5bc2c93755b45d
jay <jay_mistry@live.co.uk>
2024-08-23 21:38:01 +0100

    added to the hero section

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,9 +1,80 @@
+import Image from "next/image";
 import styles from "@/styles/components/home/hero.module.scss";
+import { linkData } from "./hero_data";
 
 export default function Hero() {
   return (
     <div className={styles.heroWrapper}>
-      <div></div>
+      <div className={styles.introCont}>
+        <p>
+          <BashPrompt /> cat intro.txt
+        </p>
+        <p>
+          Hi, I{"'"}m Jay!
+          <br />
+          <br />
+        </p>
+
+        <p>
+          <BashPrompt /> cat whoami.txt
+        </p>
+        <p>
+          I{"'"}m a Software engineer with experience in full stack development.
+          <br />I{"'"}m also interested in low level development, hardware and
+          ML!!
+          <br />
+          <br />
+        </p>
+      </div>
+      <div className={styles.linkContainer}>
+        {linkData.map(({ text, link, image }, i) => (
+          // <div className={styles.linkBox} key={i}>
+          <a
+            key={i}
+            className={styles.linkBox}
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={image}
+              alt={text}
+              width={30}
+              height={30}
+              layout="fixed"
+            />
+            <p className={styles.linkText}>{text}</p>
+          </a>
+          // </div>
+        ))}
+      </div>
     </div>
+  );
+}
+
+function BashPrompt() {
+  return (
+    <span
+      style={{
+        fontWeight: "bold",
+      }}
+    >
+      <span
+        style={{
+          color: "#16C60C",
+        }}
+      >
+        home@jellyware
+      </span>
+      :
+      <span
+        style={{
+          color: "#3B78FF",
+        }}
+      >
+        ~
+      </span>
+      $
+    </span>
   );
 }
---
+++ src/components/Home/Hero/hero_data.ts
@@ -0,0 +1,18 @@
+interface LinkData {
+  text: string;
+  link: string;
+  image: string;
+}
+
+export const linkData: LinkData[] = [
+  {
+    text: "github.com/jaym-01",
+    link: "https://github.com/jaym-01",
+    image: "/github_logo.png",
+  },
+  {
+    text: "linkedin.com/in/jay-mistry1/",
+    link: "https://www.linkedin.com/in/jay-mistry1/",
+    image: "/linkedin_logo.png",
+  },
+];
