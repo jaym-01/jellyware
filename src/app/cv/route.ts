bbef9826c6cbedec491fc993d9d07f75b3e5b8cf
jay <jay_mistry@live.co.uk>
2025-09-25 21:57:26 +0100

    changed CV

--- src/app/cv/route.ts
+++ src/app/cv/route.ts
@@ -3,14 +3,14 @@ import fs from "fs";
 import path from "path";
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), "public", "cv.pdf");
+  const filePath = path.join(process.cwd(), "public", "JayMistryCV.pdf");
 
   const fileBuffer = fs.readFileSync(filePath);
 
   return new NextResponse(fileBuffer, {
     headers: {
       "Content-Type": "application/pdf",
-      "Content-Disposition": 'inline; filename="sample.pdf"',
+      "Content-Disposition": 'inline; filename="JayMistryCV.pdf"',
     },
   });
 }
